1bb5a46c88df1be45edc0994e1e4e70fffe37905
TripleT511 <0306191074@caothang.edu.vn>
2021-10-23 10:56:03 +0700

    Update css

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 const toggle = document.querySelector(".toggle__theme");
 const card = document.querySelector(".app");
 toggle.addEventListener("click", () => {
-    let theme = toggle.querySelector(".fa");
-    if (theme.classList.contains("fa-moon-o")) {
-        theme.classList.remove("fa-moon-o");
-        theme.classList.add("fa-sun-o");
+    let theme = toggle.querySelector(".fas");
+    if (theme.classList.contains("fa-moon")) {
+        theme.classList.remove("fa-moon");
+        theme.classList.add("fa-sun");
         card.classList.add("dark");
     } else {
 
-        theme.classList.remove("fa-sun-o");
-        theme.classList.add("fa-moon-o");
+        theme.classList.remove("fa-sun");
+        theme.classList.add("fa-moon");
         card.classList.remove("dark");
     }
 })
